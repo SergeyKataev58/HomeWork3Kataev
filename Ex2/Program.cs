549acc2b2b189cc633fd6733420a18b50f412fa4
Сергей <@DESKTOP-5JQRDG9>
2021-08-10 13:50:06 +0300

    из

--- Ex2/Program.cs
+++ Ex2/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 
-namespace Ex2
+namespace Les4Kataev  //Задача сумма положительных нечетных числе введенных с клавиатуры
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int sum = 0;
+            int num = 0;
+            Console.WriteLine("Введите числа");
+            num = Sum(ref sum);
+            Console.WriteLine("Сумма положительных нечетных чисел= " + sum);
+        }
+
+        private static int Sum(ref int sum)
+        {
+            int num;
+            string a;
+            do
+            {
+                a = Console.ReadLine();
+               bool result = int.TryParse(a, out num);
+                if (result)
+                {
+
+                    if (num > 0 && num % 2 == 1)
+                        sum += num;
+                }
+                else Console.WriteLine("Число не корректно");
+                           
+            } while (num != 0);
+            return num;
+               
         }
     }
 }
